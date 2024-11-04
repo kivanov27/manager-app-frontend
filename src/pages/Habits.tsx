51ddab985d16cd85acc6c46c5dd0f5f314217c0c
fdd <kristiyan.ivanov.98@gmail.com>
2024-11-04 16:06:44 +0200

    still working on habit table rendering

--- src/pages/Habits.tsx
+++ src/pages/Habits.tsx
@@ -1,7 +1,12 @@
-import { Button, Modal, Paper, Table, TableBody, TableContainer, TableHead } from "@mui/material";
+import { useState } from "react";
+import { Button, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { Habit, NewHabit } from "../types";
 import HabitForm from "../components/HabitForm";
-import { useState } from "react";
+import dayjs from "dayjs";
+import isoWeek from "dayjs/plugin/isoWeek";
+import { daysOfWeek } from "../constants";
+
+dayjs.extend(isoWeek);
 
 interface HabitsProps {
     habits: Habit[];
@@ -18,6 +23,24 @@ const Habits = ({ habits }: HabitsProps) => {
         closeForm();
     };
 
+    const startOfYear = dayjs().startOf("year");
+    const currentDate = dayjs();
+    const weeksSinceStartOfYear = currentDate.isoWeek() - startOfYear.isoWeek() + 1;
+
+    const generateMonthHeaders = () => {
+        const headers: { month: string, colSpan: number }[] = [];
+        let currentMonth = startOfYear;
+        while (currentMonth.isBefore(currentDate)) {
+            const daysInMonth = currentMonth.daysInMonth();
+            const weeksInMonth = Math.ceil(daysInMonth / 7);
+            headers.push({ month: currentMonth.format("MMMM"), colSpan: weeksInMonth });
+            currentMonth = currentMonth.add(1, "month");
+        }
+        return headers;
+    };
+
+    const monthHeaders = generateMonthHeaders();
+
     return (
         <div className="w-full p-6">
             <h1 className="text-2xl text-center mb-10">Habits</h1>
@@ -27,8 +50,29 @@ const Habits = ({ habits }: HabitsProps) => {
                     <TableContainer component={Paper} sx={{ maxHeight: 90, minHeight: 90 }}>
                         <Table sx={{ maxHeight: 90, minHeight: 90 }} size="small" aria-label={`${habit.name}-tracker`} className='bg-[#474747]'>
                             <TableHead>
+                                <TableRow>
+                                    <TableCell />
+                                    {monthHeaders.map((header, index) => (
+                                        <TableCell key={index} align="center" colSpan={header.colSpan} className="font-bold">
+                                            {header.month}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
                             </TableHead>
+
                             <TableBody>
+                                {daysOfWeek.map((day, dayIndex) => (
+                                    <TableRow key={dayIndex}>
+                                        <TableCell>{day}</TableCell>
+                                        {[...Array(weeksSinceStartOfYear)].map((_, weekIndex) => {
+                                            const isCompleted = Math.random() > 0.5;
+                                            const cellColor = isCompleted ? "#4caf50" : "e0e0e0";
+                                            return (
+                                                <TableCell key={weekIndex} className={`bg-[${cellColor}] w-3 h-3`} />
+                                            );
+                                        })}
+                                    </TableRow>
+                                ))}
                             </TableBody>
                         </Table>
                     </TableContainer>
