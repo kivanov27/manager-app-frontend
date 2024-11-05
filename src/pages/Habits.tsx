a136e37df8fa9e70daa0e4d84b5d5e3c4db73052
fdd <kristiyan.ivanov.98@gmail.com>
2024-11-05 16:04:54 +0200

    using Dayjs to generate the table, need to add some kind of day identifier cause now I just know the first day of the week

--- src/pages/Habits.tsx
+++ src/pages/Habits.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { Button, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-import { Habit, NewHabit } from "../types";
-import HabitForm from "../components/HabitForm";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import isoWeek from "dayjs/plugin/isoWeek";
+import { useAppDispatch } from "../hooks";
+import { createHabit } from "../reducers/habitReducer";
+import { Habit, NewHabit } from "../types";
 import { daysOfWeek } from "../constants";
+import HabitForm from "../components/HabitForm";
 
 dayjs.extend(isoWeek);
 
@@ -15,46 +17,74 @@ interface HabitsProps {
 const Habits = ({ habits }: HabitsProps) => {
     const [formOpen, setFormOpen] = useState<boolean>(false);
 
+    const dispatch = useAppDispatch();
     const openForm = () => setFormOpen(true);
     const closeForm = () => setFormOpen(false);
 
     const submitHabit = async (values: NewHabit) => {
-        console.log('New Habit:', values);
+        await dispatch(createHabit(values));
         closeForm();
     };
 
     const startOfYear = dayjs().startOf("year");
     const currentDate = dayjs();
-    const weeksSinceStartOfYear = currentDate.isoWeek() - startOfYear.isoWeek() + 1;
+    const weeks: Dayjs[] = [];
+    let weekStart = startOfYear.startOf("isoWeek");
 
+    while (weekStart.isBefore(currentDate)) {
+        weeks.push(weekStart);
+        weekStart = weekStart.add(1, "week");
+    }
+    
     const generateMonthHeaders = () => {
         const headers: { month: string, colSpan: number }[] = [];
-        let currentMonth = startOfYear;
-        while (currentMonth.isBefore(currentDate)) {
-            const daysInMonth = currentMonth.daysInMonth();
-            const weeksInMonth = Math.ceil(daysInMonth / 7);
-            headers.push({ month: currentMonth.format("MMMM"), colSpan: weeksInMonth });
-            currentMonth = currentMonth.add(1, "month");
+        let currentMonth = startOfYear.startOf("month");
+        let colSpan = 0;
+
+        for (const week of weeks) {
+            if (week.month() === currentMonth.month()) {
+                colSpan++;
+            }
+            else {
+                if (colSpan > 0) {
+                    headers.push({ month: currentMonth.format("MMMM"), colSpan });
+                }
+                currentMonth = week.startOf("month");
+                colSpan = 1;
+            }
         }
+
+        // add last month's header
+        if (colSpan > 0) {
+            headers.push({ month: currentMonth.format("MMMM"), colSpan });
+        }
+        
+        // if January doesn't start on Monday add 1 empty header
+        if (startOfYear.isoWeekday() !== 1) {
+            headers.unshift({ month: "", colSpan: 1 });
+        }
+
         return headers;
     };
 
     const monthHeaders = generateMonthHeaders();
+
+    console.log(weeks);
 
     return (
         <div className="w-full p-6">
             <h1 className="text-2xl text-center mb-10">Habits</h1>
             {habits.map(habit =>
                 <div key={habit.id}>
-                    <h2>{habit.name}</h2>
-                    <TableContainer component={Paper} sx={{ maxHeight: 90, minHeight: 90 }}>
-                        <Table sx={{ maxHeight: 90, minHeight: 90 }} size="small" aria-label={`${habit.name}-tracker`} className='bg-[#474747]'>
+                    <h2 className="capitalize mb-4">{habit.name}</h2>
+                    <TableContainer sx={{ width: 'fit-content' }} component={Paper} className="mb-10">
+                        <Table sx={{ width: 'fit-content' }} size="small" aria-label={`${habit.name}-tracker`} className='bg-[#474747] border border-white'>
                             <TableHead>
                                 <TableRow>
                                     <TableCell />
                                     {monthHeaders.map((header, index) => (
-                                        <TableCell key={index} align="center" colSpan={header.colSpan} className="font-bold">
-                                            {header.month}
+                                        <TableCell key={index} align="center" sx={{ color: 'white' }} colSpan={header.colSpan} className="font-bold border border-white">
+                                            {header.month.slice(0, 3)}
                                         </TableCell>
                                     ))}
                                 </TableRow>
@@ -63,17 +93,22 @@ const Habits = ({ habits }: HabitsProps) => {
                             <TableBody>
                                 {daysOfWeek.map((day, dayIndex) => (
                                     <TableRow key={dayIndex}>
-                                        <TableCell>{day}</TableCell>
-                                        {[...Array(weeksSinceStartOfYear)].map((_, weekIndex) => {
+                                        <TableCell sx={{ color: 'white' }}>{day}</TableCell>
+                                        {weeks.map((week, weekIndex) => {
                                             const isCompleted = Math.random() > 0.5;
                                             const cellColor = isCompleted ? "#4caf50" : "e0e0e0";
                                             return (
-                                                <TableCell key={weekIndex} className={`bg-[${cellColor}] w-3 h-3`} />
+                                                <TableCell 
+                                                    id={weekIndex.toString()}
+                                                    key={weekIndex} 
+                                                    sx={{ width: '2rem', height: '2rem', maxWidth: '2rem', maxHeight: '2rem', backgroundColor: cellColor }} 
+                                                    className='border border-white' 
+                                                >{}</TableCell>
                                             );
                                         })}
                                     </TableRow>
                                 ))}
-                            </TableBody>
+                           </TableBody>
                         </Table>
                     </TableContainer>
                 </div>
